54f50c20c3ebc92aeeb9cc1a0692537603b9a74b
Labor <a@b.c>
2022-12-06 14:25:07 +0100

    drg

--- Aufgabenblatt6/src/Schiffeversenken.java
+++ Aufgabenblatt6/src/Schiffeversenken.java
@@ -11,10 +11,47 @@ public class Schiffeversenken {
         int breite = einlesen.leseInteger();
 
         int spielfeld[][] = new int[breite][breite];
+                for (int zaehler = 0; zaehler < spielfeld.length; zaehler++) {
+                    System.out.print("  " + zaehler + " ");
+                }
+                System.out.println();
+                for (int x = 0; x < spielfeld.length; x++) {
+                    for (int ersteZeile = 0; ersteZeile < breite; ersteZeile++) {
+                        System.out.print("+---");
+                    }
+                    System.out.print("+");
+                    System.out.println();
+                    for (int y = 0; y < spielfeld.length; y++) {
 
-        for (int zeile = 0; zeile < breite; zeile++) {
-
+                        switch (spielfeld[x][y]) {
+                            case 0:
+                                System.out.print("|   ");
+                                break;
+                            case 1:
+                                System.out.print("| 1 ");
+                                break;
+                            case 2:
+                                System.out.print("| 2 ");
+                                break;
+                            case 3:
+                                System.out.print("| 3 ");
+                                break;
+                            default:
+                                System.out.println("oops, something went wrong");
+                                break;
+                        }
+                    }
+                    System.out.print("|");
+                    System.out.print(" " + x);
+                    System.out.println();
+                }
+                for (int letzteZeile = 0; letzteZeile < breite; letzteZeile++) {
+                    System.out.print("+---");
+                }
+                System.out.print("+");
+                System.out.println();
+            }
 
         }
-    }
-}
+
+
